f92ff2bb2157423273c2a3ce6911f34fbf248036
sisso <sisso@sisso.com.br>
2013-09-05 11:17:20 -0300

    - creating test for android

---
+++ Assets/UnityRate/Scripts/UnityRate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Runtime.InteropServices;
+
+public class UnityRate {
+	private string prefsPrefixe = "unityrate";
+	
+	private string appName = "undefinedAppName";
+	
+	public void IncreaseCount() {
+		var amount = int.Parse(PlayerPrefs.GetString(prefsPrefixe+".count", "0"));
+		PlayerPrefs.SetString(prefsPrefixe+".count", (amount+1).ToString());
+	}
+	
+	public void SendToRating() {
+		Debug.Log("SendToRating.started");
+#if UNITY_ANDROID		
+		AndroidJavaClass appClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject activity = appClass.GetStatic<AndroidJavaObject>("currentActivity");
+		
+		// uri = Uri.parse("market://details?id=" + APP_PNAME)
+		AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+		AndroidJavaObject uri = uriClass.CallStatic<AndroidJavaObject>("parse", "market://details?id=" + appName);
+		
+		// intent = new Intent(Intent.ACTION_VIEW, uri)
+		AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+		AndroidJavaObject intent = new AndroidJavaObject("android.content.Intent", intentClass.GetStatic<AndroidJavaObject>("ACTION_VIEW"), uri);
+		
+		// activity.startActivity(intent);
+		activity.Call("startActivity", intent);
+#endif
+		Debug.Log("SendToRating.finished");
+	}
+}
---
+++ Assets/UnityRate/Scripts/UnityRateTestGui.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityRateTestGui : MonoBehaviour {
+	private UnityRate rate;
+	private string msg = "Wel come";
+	
+	void OnGUI() {
+		var w = Screen.width;
+		var h = Screen.height/5;
+		GUILayout.BeginArea(new Rect(0,0, w, Screen.height));
+		GUILayout.Label(msg);
+		if (GUILayout.Button("Create", GUILayout.Height(h))) {
+			rate = new UnityRate();
+			msg = "Created";
+		}
+		if (GUILayout.Button("Send to rating", GUILayout.Height(h))) {
+			rate.SendToRating();
+			msg = "Sent";
+		}
+		
+		GUILayout.EndArea();
+	}
+}
